aa5296205ca454d5bd830c8fce9ce5ad938634eb
Javeria-Tareen <javeriatareen461@gmail.com>
2026-01-07 23:45:24 +0500

    patch

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.use(cors({
     return callback(new Error(`CORS blocked for origin: ${origin}`));
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
